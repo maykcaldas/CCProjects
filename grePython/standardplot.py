55551d7e71c68cb774ec15e93840546d336a0c98
maykcaldas <maykcaldas@gmail.com>
2019-07-09 15:16:19 -0300

    organizing sorting repository

--- grePython/standardplot.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x=np.linspace(1,20,100)
-plt.plot(x,np.sin(x))
-plt.show()
--- sorting/sorting.py
+++ sorting/sorting.py
@@ -3,6 +3,32 @@
 import os
 #import numpy as np
 
+def mergeSort():
+    pass
+
+
+def insertionSort():
+    pass
+
+
+def bubbleSort():
+    pass
+
+
+
+def quickSort():
+    pass
+
+
+
+def heapSort():
+    pass
+
+
+def countingSort():
+    pass
+
+
 def sorting(listToSort):
     sort=[]
     for i in range(len(listToSort)):
@@ -16,36 +42,28 @@ def sorting(listToSort):
         listToSort[i], listToSort[min] = listToSort[min], listToSort[i]
 
         
-        # for i in range(len(listToSort)):
-        #     if listToSort[j] < listToSort[min]:
-        #         min = j
-        # sort.append(listToSort[min])
-        # listToSort[i], listToSort[min] = listToSort[min], listToSort[i]
-        
-
-
     return sort
 
 
 def main():
-    inp = input("Insira uma tupla de números: ")
-    
-    stringInput=inp.split()
-    listToSort=[]
+    #inp = input("Insira uma tupla de números: ")
+    #
+    #stringInput=inp.split()
+    #listToSort=[]
 
-    if all([k.isdigit() for k in stringInput]) is True:
-        for d in stringInput:
-            listToSort.append(float(d))
-            
+    #if all([k.isdigit() for k in stringInput]) is True:
+    #    for d in stringInput:
+    #        listToSort.append(float(d))
+    #        
 
-        print("{0:^20s}: {1}".format("A entrada foi", listToSort))
-        print("{0:^20s}: {1}".format("Função sorted()", sorted(listToSort)))
-        print("{0:^20s}: {1}".format("Subrotina sorting()", sorting(listToSort)))
+    #    print("{0:^20s}: {1}".format("A entrada foi", listToSort))
+    #    print("{0:^20s}: {1}".format("Função sorted()", sorted(listToSort)))
+    #    print("{0:^20s}: {1}".format("Subrotina sorting()", sorting(listToSort)))
 
-    else:
-        print("Inserir somente números separados por espaços")
+    #else:
+    #    print("Inserir somente números separados por espaços")
 
 
 
 main()
-os.system("pause")
+#os.system("pause")
