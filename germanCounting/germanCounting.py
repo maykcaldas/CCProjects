05d708eb31bd6063b1286b620b19621c655a0185
Kyam <maykcaldas@gmail.com>
2019-08-14 23:02:51 -0300

    project to german cards: number 0-100

---
+++ germanCounting/germanCounting.py
@@ -0,0 +1,65 @@
+#encoding: UTF-8
+
+import os
+import random
+
+base=["null",
+        "ein",
+        "zwei",
+        "drei",
+        "vier",
+        "fünf",
+        "sechs",
+        "sieben",
+        "acht",
+        "neun",
+        "zhen"]
+
+key=1
+while (key != "0"):
+    number = str(random.randint(0,100))
+    print(number, end="")
+    key = input("")
+    if number == "1":
+        print(" - eins")
+    elif len(number)==1:
+        print(base[int(number)])
+    elif len(number)==2:
+        if number[1]=="0":
+            if number[0]=="1":
+                print(" - "+base[int(number)])
+            elif number[0]=="6":
+                print(" - sechzig")
+            elif number[0]=="7":
+                print(" - siebzig")
+            elif number[0]=="3":
+                print(" - dreiszig")
+            elif number[0]=="2":
+                print(" - zwanzig")
+            else:
+                print(" - " + base[int(number[0])]+"zig")
+        elif number[0]=="1":
+            if number[1]=="1":
+                print(" - elf")
+            elif number[1]=="2":
+                print(" - zwölf")
+            elif number[1]=="6":
+                print(" - sechzhen")
+            elif number[1]=="7":
+                print(" - siebzhen")
+            else:
+                print(" - " + base[int(number[1])]+"zhen")
+        elif number[0]=="2":
+                print(" - " + base[+int(number[1])]+"undzwanzig")
+        elif number[0]=="6":
+            print(" - " + base[+int(number[1])]+"undsechzig")
+        elif number[0]=="7":
+            print(" - " + base[int(number[1])]+"undsiebzig")
+        elif number[0]=="3":
+            print(" - " + base[int(number[1])]+"unddreiszig")
+        else:
+            print(" - " + base[int(number[1])]+"und"+base[int(number[0])]+"zig")
+    elif len(number)==3:
+        print("- ein hundert")
+
+
