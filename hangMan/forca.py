60f9bbca7ff2cc2d271515798eebac06df16e153
maykcaldas <maykcaldas@gmail.com>
2022-01-12 14:16:13 -0300

    hangman project renamed

---
+++ hangMan/forca.py
@@ -0,0 +1,79 @@
+###################################################
+#-------------------------------------------------#
+#----------------Jogo de forca--------------------#
+#-----------Author: Mayk Caldas ------------------#
+#-------------Done in 20/02/2019------------------#
+#--------In a night before become asleepy --------#
+#-------------------------------------------------#
+###################################################
+
+
+
+#####################################
+####### CHOOSE A WORD TO PLAY #######
+#####################################
+
+palavra="bicicleta"
+
+#############################################
+## DO NOT CHANGE ANYTHING ELSE IN THE CODE ##
+#############################################
+
+
+import os
+
+erros=0
+letrasErradas=[]
+
+forca="-"*len(palavra)
+
+print(forca)
+
+while (erros < 6):
+    chute=input("Take a guess: ")
+    
+    while (chute.isalpha() != True):
+        os.system("clear")
+        print("The input should be just a character")
+        chute=input("Take a guess: ")
+
+    if chute in palavra:
+        os.system("clear")
+        for k in range(palavra.count(chute)):
+            posicao=palavra.find(chute)
+            forca = forca[0:posicao]+chute+forca[posicao+1:]
+            palavra=palavra.replace(chute,"-",1)
+        print(forca)
+        print("")
+
+        if palavra=="-"*len(palavra):
+            os.system("clear")
+            print("Yeeeeay! You won!")
+            break
+    
+    elif chute in letrasErradas or chute in forca:
+        os.system("clear")
+        print("You have already tried this one.")
+        print("You had already wrong those ones:")
+        print(letrasErradas)
+        print("\n Try again")
+        print(forca)
+        
+    else:
+        erros+=1
+        letrasErradas.append(chute)
+        
+        if erros == 6:
+            print("Just died")
+            break
+        
+        os.system("clear")
+        print("{0} mistakes! And you already wrong those ones:".format(erros))
+        print(letrasErradas)
+        print("")
+        print("Try again: ")
+        print(forca)
+        print("")
+
+#That is needed to pause when running in a windows environment
+#os.system("Pause")
